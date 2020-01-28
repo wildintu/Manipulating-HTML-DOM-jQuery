d91a624eb7f08d4e24c142e804e14b99a30b5ecf
wildintu <hello@wildintu.com>
2020-01-28 15:03:54 -0600

    DOM Finished

--- DOM/script.js
+++ DOM/script.js
@@ -27,8 +27,8 @@ document.addEventListener('DOMContentLoaded', function () {
     let div = document.createElement('div');
     document.body.appendChild(div);
     div.style.backgroundColor = 'transparent';
-    div.style.width = '300px';
-    div.style.height = '300px';
+    div.style.width = '100px';
+    div.style.height = '100px';
 
     //3. change div to blue bg when hovered over
     div.onmouseover = function () {
@@ -57,18 +57,63 @@ document.addEventListener('DOMContentLoaded', function () {
     document.body.appendChild(btn2);
     let btn2Text = document.createTextNode('Add name span to empty div');
     btn2.appendChild(btn2Text);
+    //div.appendChild(btn2);
+    let div2 = document.createElement('div');
+    document.body.appendChild(div2);
 
     btn2.onclick = function () {
         let name = document.createElement("Lexi");
         let lexi = document.createTextNode("Lexi ")
         name.appendChild(lexi);
         document.body.appendChild(name);
+        div2.appendChild(name);
+        div2.style.height = '';
+        div2.style.width = '';
     };
 
     //6. Create a button and a ul in HTML; JS - creat an array containing names of 10 friends; when button clicked, add each friends name as an li on to the ul
-    
+    let btn3 = document.createElement('button');
+    document.body.appendChild(btn3);
+    let btn3Text = document.createTextNode('List Friends');
+    btn3.appendChild(btn3Text);
+    //div.appendChild(btn3);
+    let div3 = document.createElement('div');
+    document.body.appendChild(div3);
 
 
+
+
+    btn3.onclick = function () {
+        let ul = document.createElement('ul');
+        document.body.appendChild(ul);
+        // let li = document.createElement("li");
+        //console.log(liFriends());
+        //let liText = document.createTextNode(friends[i]);
+        //let lilText = liFriends();
+        //let liText = document.createTextNode('Abby','Betty','Carrie','Drake','Edward','Frank','Gary','Hunter','Ian','Jane');
+        //ul.appendChild(li);
+        //li.appendChild(liText);
+        //div3.appendChild(li); 
+        //let liText = document.createTextNode(friends[i]);
+        let friends = ['Abby', 'Betty', 'Carrie', 'Drake', 'Edward', 'Frank', 'Gary', 'Hunter', 'Ian', 'Jane']
+        //function liFriends(i) {
+            for (i = 0; i < friends.length; i++) {
+            let li = document.createElement("li");
+            li.textContent = friends[i];
+            ul.appendChild(li);
+            }
+        //}
+        //document.body.appendChild(ul);
+    }
+
+    // let friends = ['Abby','Betty','Carrie','Drake','Edward','Frank','Gary','Hunter','Ian','Jane']
+    // function liFriends(i) {
+    // for(i = 0; i < friends.length; i++);
+    // let li = document.createElement("li");
+    // li.textContent = friends[i];
+    // ul.appendChild(li);
+    // }
+    //li.appendChild(liText);
 });
 
 //4. random color background function
@@ -76,3 +121,9 @@ function getColor() {
     let randomColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
     return randomColor;
 };
+
+// let friends = ['Abby','Betty','Carrie','Drake','Edward','Frank','Gary','Hunter','Ian','Jane']
+// function liFriends() {
+//     for(i = 0; i < friends.length; i++);
+//     return friends[i];
+// }
